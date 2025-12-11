78d301e42485db1f6572a85f0e073ae83ea404c4
650462636292 <153263126+6504062636292@users.noreply.github.com>
2025-12-11 22:44:29 +0700

    Refactor formatting and improve VAT output message

--- lab1.3.c
+++ lab1.3.c
@@ -3,19 +3,19 @@
 int main() {
     float a, b, c;
     
-    scanf("%f", &a);
-    float p1 = a * 0.95;
-    printf("%.2f\n", p1);
+    scanf("%f",&a);
+    float p1 = a*0.95;
+    printf("%.2f\n",p1);
 
     scanf("%f", &b);
-    float p2 = (a + b) * 0.85;
-    printf("%.2f\n", p2);
+    float p2 = (a+b)*0.85;
+    printf("%.2f\n",p2);
 
     scanf("%f", &c);
-    float p3 = (a + b + c) * 0.70;
-    printf("%.2f\n", p3);
+    float p3 = (a+b+c)*0.70;
+    printf("%.2f\n",p3);
 
-    printf("vat = %.2f", p3 * 1.07);
+    printf("Vat = %.2f",p3*1.07);
 
     return 0;
 }
